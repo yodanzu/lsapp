cc715df61d291a5d2b04d3f8f9d8bdbe98bf8eb8
yodanzu <rivadelomij@gmail.com>
2019-11-26 18:05:03 +0800

    SAMPLE

--- routes/web.php
+++ routes/web.php
@@ -26,20 +26,6 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
-/*Route::get('/courses', 'CoursesController@index');
-Route::get('/courses/create', 'CoursesController@create');
-Route::get('/courses/{id}', 'CoursesController@show');
-Route::post('/courses', 'CoursesController@store');
-Route::put('/posts/edit', 'CoursesController@update');
-Route::delete('/courses/destroy', 'CoursesController@destroy');
-    
-Route::get('/posts', 'PostsController@index');
-Route::get('/posts/create', 'PostsController@create');
-Route::get('/posts/{id}', 'PostsController@show');
-Route::post('/posts', 'PostsController@store');
-Route::put('/posts/edit', 'PostsController@update');
-Route::delete('/posts/{id}/destroy', 'PostsController@destroy');*/
-
 Route::resource('courses', 'CoursesController');
 Route::resource('posts', 'PostsController');
 Route::resource('scheds', 'SchedsController');
